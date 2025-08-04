23a3b51fc5c8ac7094acc44969c938b11e8480f2
GowthamSaiKorada <2300032467@kluniversity.in>
2025-08-04 10:44:28 +0530

    modifed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'jenkinsrerp' //deployment folder name but not a git repo
+  base:'jenkinsrerp', //deployment folder name but not a git repo
 })
